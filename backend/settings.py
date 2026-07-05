72692150139d3ba93b1f052b22433935caa17532
sahilchoudhary2117 <madinachoudhary2117@gmail.com>
2026-07-05 16:20:24 +0530

    Use environment variables for settings

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5able2eem+)-@e7s)97!(!w$ql#z+xp$6*agyfe7jts#_c&*3v'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-5able2eem+)-@e7s)97!(!w$ql#z+xp$6*agyfe7jts#_c&*3v"
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
