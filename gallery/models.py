8d210ad7f8019aa52ae0143edbb0856825a225c7
sahilchoudhary2117 <madinachoudhary2117@gmail.com>
2026-07-06 16:46:44 +0530

    web tab name of the site

--- gallery/models.py
+++ gallery/models.py
@@ -17,9 +17,9 @@ class Photo(models.Model):
 
     description=models.TextField()
 
-    image=models.ImageField(upload_to="original/")
+    image = models.TextField()
 
-    edited_image=models.ImageField(upload_to="edited/", blank=True)
+    edited_image = models.TextField(blank=True)
 
     tags=models.CharField(max_length=200)
 
